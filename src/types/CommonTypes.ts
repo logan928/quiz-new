504e4a8c89c73ef73caee69cef2fedcec1bb7c5e
upulera <upul.eranda@gmail.com>
2024-09-29 20:46:22 +0200

    add types

---
+++ src/types/CommonTypes.ts
@@ -0,0 +1,10 @@
+//Type for question
+export interface Question {
+  id: number;
+  question_type: "mcq" | "short_answer";
+  prompt: string;
+  correct_answer: string;
+  wrong_answers?: string[];
+  score: number;
+  difficulty: "easy" | "medium" | "difficult";
+}
